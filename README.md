6bd6fdde8897bae6ac8f42a57b7f60695216ab70
idanran <idanran@outlook.com>
2023-10-14 18:40:47 +0800

    1.1.0

--- README.md
+++ README.md
@@ -16,8 +16,10 @@ yarn build
 ```
 然后把编码器 (cli.exe) 放入 lib 目录。
 
-## Build `cli.exe`
+## Build cli
 ```
 cd rs
 cargo build --release
+cross build --target x86_64-unknown-linux-musl
 ```
+
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import { spawn } from 'child_process'
 import { join } from 'path'
 import { access, constants } from 'fs'
+import { platform } from 'os'
 
 export function encode(input: string, output: string, samplingRate: string): Promise<void> {
     return new Promise((resolve, reject) => {
+        let bin = './cli.exe'
+        if (platform() === 'linux') {
+            bin = './cli'
+        }
         const args = ['-i', input, '-s', samplingRate, '-o', output]
-        const child = spawn(join(__dirname, './cli.exe'), args)
+        const child = spawn(join(__dirname, bin), args)
         child.on('exit', () => {
             access(output, constants.F_OK, (err) => {
                 if (err) {
